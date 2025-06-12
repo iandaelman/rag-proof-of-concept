81ed2c300d9c7ca55b19e2288291b35293863e7e
Ian Daelman <iandaelman@hotmail.com>
2025-06-12 16:51:58 +0200

    removed 3 sentence constriction in system prompt

--- app/retriever.py
+++ app/retriever.py
@@ -22,8 +22,7 @@ def retrieve(state: State):
 
 def generate(state: State):
     prompt = ChatPromptTemplate.from_template("""
-    You are an assistant for question-answering tasks. Use the following pieces of retrieved context to answer the question. If you don't know the answer, just say that you don't know. Use three sentences maximum and keep the answer concise.
-
+    You are an assistant for question-answering tasks. Use the following pieces of retrieved context to answer the question. If you don't know the answer, just say that you don't know.
     Question: {question}
 
     Context: {context}
